0cde8865d7d17b0823ef489d2d32b13f695c7716
Eshan <astrobytesgaming@gmail.com>
2026-05-08 15:59:27 -0700

    chore: update config.js with mongo connection url

--- config.js
+++ config.js
@@ -3,7 +3,7 @@ require('dotenv').config();
 module.exports = {
     TOKEN: process.env.TOKEN,
     APP_ID: "1502139583557533857",
-    MONGOURL: "",
+    MONGOURL: "mongodb+srv://notastrobytes_db_user:king@blossom.qggzx8i.mongodb.net/",
     GUILD_ID: "1502137646946848902",
     prefix: "-"
 };
