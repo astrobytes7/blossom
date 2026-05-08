4ed471a717a9bfe236fc98d6838228b828804bf1
Eshan <astrobytesgaming@gmail.com>
2026-05-08 15:58:02 -0700

    feat: make tax commands use embeds with fields and image

--- commands/tax.js
+++ commands/tax.js
@@ -1,5 +1,5 @@
 const { SlashCommandBuilder } = require('@discordjs/builders');
-const { PermissionsBitField } = require('discord.js');
+const { EmbedBuilder } = require('discord.js');
 
 module.exports = {
     data: new SlashCommandBuilder()
@@ -11,14 +11,21 @@ module.exports = {
                 .setRequired(true)
         ),
     execute: async (interaction, client) => {
-
-
         const amount = interaction.options.getInteger('amount');
-        const receive = Math.floor(amount * 0.7);
         const charge = Math.ceil(amount / 0.7);
+        const taxAmount = charge - amount;
+
+        const embed = new EmbedBuilder()
+            .setColor('#ff66c4')
+            .addFields(
+                { name: 'Before Tax', value: `${amount}`, inline: true },
+                { name: 'Tax', value: `${taxAmount}`, inline: true },
+                { name: 'After Tax', value: `${charge}`, inline: true }
+            )
+            .setImage('https://media.discordapp.net/attachments/1264584269758468249/1408682021802348634/Blossom_Customs_footer_better.png?ex=69ff6c9e&is=69fe1b1e&hm=8b6c5258cd963692c66ac13e122f977080145eae0d16bc82becfcddae298a8a5&=&format=webp&quality=lossless&width=2834&height=218');
 
         await interaction.reply({
-            content: `<:calculator:1502442136711200829> The taxed amount is **${charge}**.`,
+            embeds: [embed]
         });
     }
 };
