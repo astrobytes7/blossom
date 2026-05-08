4c380937db468711ddb24ef7367ddd1ec1ed3c97
Eshan <astrobytesgaming@gmail.com>
2026-05-07 20:37:34 -0700

    Initial commit

---
+++ config.js
@@ -0,0 +1,9 @@
+require('dotenv').config();
+
+module.exports = {
+    TOKEN: process.env.TOKEN,
+    APP_ID: "1502139583557533857",
+    MONGOURL: "",
+    GUILD_ID: "1502137646946848902",
+    prefix: "-"
+};
